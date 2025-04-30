34c3302f3c97377ff7b5f499441d61ca9f535cb2
Neon <neon@neosynth.net>
2025-04-30 12:05:17 -0700

    cleaned up configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,21 +1,25 @@
 { pkgs, ... }: {
-  imports = [ ./nixvim ];
 
-  programs.nixvim.enable = true;
-
+  # Standard nix configuration stuff
   nix.settings.experimental-features = "nix-command flakes";
-
   system.configurationRevision = null; # Will be set from flake
   system.stateVersion = 5;
-  nix.enable = false;
+  nix.enable = false; # We are using determinate nix, so we want this false
   nixpkgs.hostPlatform = "aarch64-darwin";
 
+  # Use nvim configuration from nixvim module
+  imports = [ ./nixvim ];
+  programs.nixvim.enable = true;
+
+  # Menubar Settings
   system.defaults.menuExtraClock.Show24Hour = true;
   system.defaults.controlcenter.BatteryShowPercentage = true;
 
-  system.defaults.dock.autohide = true;
-  system.defaults.dock.autohide-time-modifier = 0.15;
+  # Dock Settings
+  # Nix version of this command: defaults write com.apple.dock autohide-delay -float 0; killall Dock
+  #system.defaults.dock.autohide-delay = 0;
 
+  # Finder Settings
   system.defaults.finder = {
     ShowPathbar = true;
     ShowStatusBar = true;
@@ -25,12 +29,15 @@
     FXRemoveOldTrashItems = true;
     FXPreferredViewStyle = "clmv";
   };
-
+  
+  # Other misc preferences
   system.defaults.CustomUserPreferences = {
+    # # Screenshots to /tmp
     "com.apple.screencapture" = {
       location = "/private/tmp";
       type = "png";
     };
+    # Disable pinentry save to keychain
     "org.gpgtools.common" = {
       "UseKeychain" = "NO";
     };
